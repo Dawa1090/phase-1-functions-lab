7378fd936407954c8bf3843e600d7976d08e802c
Dawa Sherpa <dawasherpa1090@gmail.com>
2023-07-30 10:53:39 -0400

    1st question

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  // Scuber headquarters is on 42nd Street.
+  const hq_location = 42;
+  const distance_blocks = Math.abs(pickupLocation - hq_location);
+  return distance_blocks;
+}
